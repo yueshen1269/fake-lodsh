56c04bd6c80b8e08ecebe04e2d13e37218c94af7
yueshen1269 <1152555377@qq.com>
2019-11-25 11:03:21 +0800

    改进chunk函数

--- chunk.js
+++ chunk.js
@@ -2,7 +2,7 @@
 // 时间：2019年11月25日10:08:25
 function _chunk(array, size = 1) {
   if(size<=0) throw new Error("size has to be greater than 0");
-  if(Array.isArray(array)) throw new Error("array has to be an array");
+  if(!Array.isArray(array)) throw new Error("array has to be an array");
   if(size===1) return array.slice();
   const len = array.length;
   const result = [];
@@ -13,6 +13,26 @@ function _chunk(array, size = 1) {
       result.push(temp);
       temp = [];
     }
+  }
+  return result;
+}
+
+// 第二次实现_chunk函数， 2019年11月25日10:39:33
+// 看了源码后的改进：
+// 1. 处理size类型为整形，并且异常值的情况下输出边界值[]，不再抛出异常
+// 2. result数组长度计算
+// 3. 使用while代替for循环，整体赋值，不用push并取余判断
+function _chunk2(array, size = 1) {
+  size = Math.max(parseInt(size), 0);
+  if(!size || !Array.isArray(array) || !array.length) return [];
+  if(size===1) return array.slice();
+  const len = array.length;
+  const result = new Array(Math.ceil(len / size));
+  let index = 0,
+      rstIndex = 0;
+  while(index < len) {
+    result[rstIndex++] = array.slice(index, index + size);
+    index += size;
   }
   return result;
 }
